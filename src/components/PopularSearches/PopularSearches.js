162c122c8f708aadb6ac48a4dd41ad240541f03a
Jose Campo Rivas <josecampor31@gmail.com>
2022-05-28 15:04:43 +0200

    change of PopularSearches

--- src/components/PopularSearches/PopularSearches.js
+++ src/components/PopularSearches/PopularSearches.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import './PopularSearches.css'
 
-const POPULAR_SEARCHS = ['Matrix', 'Random', 'Architecture']
+const POPULAR_SEARCHS = ['matrix', 'random', 'architecture', 'nature']
 
 export default function PopularSearches() {
     return (
